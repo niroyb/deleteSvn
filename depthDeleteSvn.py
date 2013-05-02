93831df858a1e741e29318c3be167311a51a83f6
Nicolas Roy <niroyb@gmail.com>
2013-05-02 20:00:18 -0300

    re moins de code

--- depthDeleteSvn.py
+++ depthDeleteSvn.py
@@ -1,21 +1,7 @@
-import os
-import shutil
-import stat
+import os, shutil
 
-def on_rm_error( func, path, exc_info):
-    os.chmod( path, stat.S_IWRITE )
-    os.unlink( path )
-iters = 0
-def dig(path):
-    pathRoot = path+"\\"
-    for f in os.listdir(pathRoot):
-         if f.endswith(".svn"):
-             print("Efface ",pathRoot+f)
-             iters+=1
-             shutil.rmtree( pathRoot + f, onerror = on_rm_error )
-         elif os.path.isdir(pathRoot + f):
-             dig(pathRoot + f)
-         
-dig(os.getcwd())
-print("Terminé. ",iters,"fichiers effacés.")
-input("Entrez qqc pour quitter.")
+root = os.getcwd()
+for dirpath, dirnames, filenames in os.walk(root):
+    if '.svn' in dirnames:
+        absPath = os.path.join(dirpath, '.svn')
+        shutil.rmtree(absPath)
